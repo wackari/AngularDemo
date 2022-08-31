0363e318d82227abb39ef7bcc78ff13668663af3
MARDOM\mcrodriguez <marckorv87@gmail.com>
2022-08-31 17:03:13 -0400

    first commit

---
+++ src/app/child.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';  
+  
+@Component({  
+  selector: 'child',  
+  templateUrl: './child.component.html',  
+  styleUrls : ['./custom.css']  
+})  
+export class ChildComponent {  
+      
+}  
